59ccc9fefdd549514c6f990ee86bd488d70e59d0
Callum Beckwith <123callumb@gmail.com>
2021-03-07 12:45:33 +0000

    maze size changing works
    Signed-off-by: Callum Beckwith <123callumb@gmail.com>

--- src/maze_project/interface/listeners.clj
+++ src/maze_project/interface/listeners.clj
@@ -2,12 +2,25 @@
   (:use [seesaw.core])
   (:require [maze-project.interface.ui-canvas :refer [draw-maze]]))
 
-(defn on-maze-combo [e]
-  (let [mazeName (selection e)
-        frame (to-frame e)]
-    (draw-maze frame mazeName 20 20)))
+(defn get-row-slider-val [frame]
+  (selection (select frame [:#rowSlider])))
+
+(defn get-col-slider-val [frame]
+  (selection (select frame [:#colSlider])))
+
+(defn get-maze-alg-name [frame]
+  (selection (select frame [:#mazeComboSel])))
+
+(defn update-maze-draw [e]
+  (let [frame (to-frame e)
+        mazeAlgName (get-maze-alg-name frame)
+        rows (get-row-slider-val frame)
+        cols (get-col-slider-val frame)]
+    (draw-maze frame mazeAlgName rows cols)))
 
 (defn register-listeners [l]
-  (listen (select l [:#mazeComboSel]) :selection on-maze-combo))
+  (listen (select l [:#mazeComboSel]) :selection update-maze-draw)
+  (listen (select l [:#rowSlider]) :change update-maze-draw)
+  (listen (select l [:#colSlider]) :change update-maze-draw))
 
 
--- src/maze_project/interface/ui_layout.clj
+++ src/maze_project/interface/ui_layout.clj
@@ -4,10 +4,14 @@
 
 (defn create-left-panel []
   (let [mazeComboLabel (label :text "Set maze generation algorithm:")
-        mazeComboBox (combobox :id :mazeComboSel :model get-maze-names)]
+        mazeComboBox (combobox :id :mazeComboSel :model get-maze-names)
+        rowSliderLabel (label :text "Maze Rows:")
+        rowSlider (slider :id :rowSlider :min 3 :max 50 :value 20 :orientation :horizontal :minor-tick-spacing 1 :major-tick-spacing 1 :snap-to-ticks? true)
+        colSliderLabel (label :text "Maze Columns:")
+        colSlider (slider :id :colSlider :min 3 :max 50 :value 20 :orientation :horizontal :minor-tick-spacing 1 :major-tick-spacing 1 :snap-to-ticks? true)]
     (grid-panel
       :border "Properties"
       :columns 1
-      :items [mazeComboLabel mazeComboBox])))
+      :items [mazeComboLabel mazeComboBox rowSliderLabel rowSlider colSliderLabel colSlider])))
 
 (defn create-layout [] (border-panel :west (create-left-panel) :center (create-canvas)))
